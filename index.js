ac14969887521b6b737f840976600c5cb381576b
Sean Christopher Febo <scf@techie.com>
2018-07-15 01:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 } 
 
 function deleteFromDriverByKey(driver, key) { 
-  const newDriver = Object.assign({}...driver}
+  const newDriver = Object.assign({}, driver);
   delete newDriver[key];
-  return newDriver 
+  return newDriver; 
 }
